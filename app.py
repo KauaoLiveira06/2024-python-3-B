ff91fbeb7387c99b30289dd7fc50acb168a79ceb
Kauã dos Reis de Oliveira <127523958+KauaoLiveira06@users.noreply.github.com>
2024-09-05 10:40:16 -0300

    aula 05/09

---
+++ app.py
@@ -0,0 +1,9 @@
+print ('Company Papelaria e Embalagens\n')
+
+print ('1. Cadastro de produtos e mercadorias')
+print ('2. Listar produtos e mercadorias')
+print ('3. Ativar produtos e mercadorias')
+print ('4. Sair da aplicação')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print ('Você escolheu a opção: ', opcao_escolhida)
